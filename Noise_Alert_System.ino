22bfbccff5460d694db1a4d99fc169322f47c6c6
atilanorobertjohn-stack <atilanorobertjohn@gmail.com>
2026-01-22 20:49:30 +0800

    Update Noise_Alert_System.ino

--- Noise_Alert_System.ino
+++ Noise_Alert_System.ino
@@ -14,47 +14,12 @@ void loop() {
  
 
   if (analogRead(sound) > threshold) {
-    // sound DETECTED
-    digitalWrite(ledPin, HIGH);
     tone(buzzerPin, 1900);
-    delay (500);
-    digitalWrite(ledPin, LOW);
-    tone(buzzerPin, 1800);
-    delay (500);
-    digitalWrite(ledPin, HIGH);
-    tone(buzzerPin, 1900);
-    delay (500);
-   digitalWrite(ledPin, LOW);
-    tone(buzzerPin, 1800);
-    delay (500);
-    digitalWrite(ledPin, HIGH);
-    tone(buzzerPin, 1900);
-    delay (500);
-    digitalWrite(ledPin, LOW);
-    tone(buzzerPin, 1800);
-    delay (500);
-     digitalWrite(ledPin, HIGH);
-    tone(buzzerPin, 1900);
-    delay (500);
-    digitalWrite(ledPin, LOW);
-    tone(buzzerPin, 1800);
-    delay (500);
-    digitalWrite(ledPin, HIGH);
-    tone(buzzerPin, 1900);
-    delay (500);
-   digitalWrite(ledPin, LOW);
-    tone(buzzerPin, 1800);
-    delay (500);
-    digitalWrite(ledPin, HIGH);
-    tone(buzzerPin, 1900);
-    delay (500);
-    digitalWrite(ledPin, LOW);
-    tone(buzzerPin, 1800);
-    delay (500);
+   
 
   }
   else {
-    // No sound
+    
     digitalWrite(ledPin, LOW);
     noTone(buzzerPin);
   }
