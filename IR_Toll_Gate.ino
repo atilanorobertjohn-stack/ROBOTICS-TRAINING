439e166eb01a8856e71b798aafae01d4f22b2045
atilanorobertjohn-stack <atilanorobertjohn@gmail.com>
2026-01-22 20:57:54 +0800

    Update IR_Toll_Gate.ino

--- IR_Toll_Gate.ino
+++ IR_Toll_Gate.ino
@@ -1,23 +1,22 @@
 #include <Servo.h>
 
 int IR = 8;
-Servo myServo;
 
 void setup() {
   pinMode(IR, INPUT);
-  myServo.attach(9);   
-  myServo.write(0);    
+  Servo.attach(9);   
+  Servo.write(0);    
 }
 
 void loop() {
 
 
   if (digitalRead(IR) == LOW) {
-    myServo.write(90);   
+    Servo.write(90);   
     delay(3000);
   } 
   else {
-    myServo.write(0);   
+    Servo.write(0);   
     delay(500);
   }
 }
